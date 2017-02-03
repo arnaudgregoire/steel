0dc282cf6efff54b985a0356df8fe41787f656ab
Arnaud Grégoire <arnaudgregoire92@gmail.com>
2017-02-03 12:33:17 +0100

    Add files via upload

--- node/wrapper_api_geonames.js
+++ node/wrapper_api_geonames.js
@@ -1,9 +1,11 @@
 /*
 les différentes librairies utilisées
-http : pour créer le serveur
-express : pour faciliter l'écriture bas niveau de node
+http            : pour créer le serveur
+express         : pour faciliter l'écriture bas niveau de node
 fast-xml-parser : pour convertir le string xml du serveur en xml
 puis convertir ce xml en json
+url             : Avoir le nom de l'url
+querystring     : faire un dictionnaire à partir des arguments passés en GET dans l'url
 */
 var http          = require('http');
 var express       = require('express');
@@ -13,25 +15,30 @@ var querystring   = require('querystring');
 var app           = express();
 
 
+// on créé le serveur
 app.get("/", function (req, res) {
     var params = querystring.parse(url.parse(req.url).query);
-  // on ouvre un serveur
+	// on ouvre un serveur
     var options = {
+		// on se connecte au serveur api.geonames 
         hostname: "api.geonames.org",
         method : 'GET',
+		// on fait appel à une fonction intermédiaire pour créér l'url
         path: create_path_geonames(params['lat'],params['lng'])
     };
 
     var gsaReq = http.get(options, function (response) {
+		// on envoit la réponse au serveur
         console.log('envoi d\' une requête au serveur');
         var completeResponse = '';
         response.on('data', function (chunk) {
+			// on construit la réponse avec ce que nous envoit le serveur geonames
             completeResponse += chunk;
         });
         response.on('end', function() {
             console.log('réponse du serveur reçu');
             var jsonObj = fastXmlParser.parse(completeResponse);
-            console.log('Convertissement XML to JSON terminé');
+            console.log('Conversion XML to JSON terminé');
             console.log(jsonObj);
             res.jsonp(JSON.stringify(jsonObj));
             res.end();
